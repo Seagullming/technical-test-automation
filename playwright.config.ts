8e1c90051e3f97c03172a71cac1f0f594130de8a
Haha Ha <lymljzlh@gmail.com>
2026-08-11 02:40:55 +1200

    imported allure report into framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,14 +22,22 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never' }],
+    [
+      'allure-playwright',
+      {
+        resultsDir: 'allure-results',
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://automationteststore.com/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-     /* Take screenshot when test getting failed */
+    /* Take screenshot when test getting failed */
     screenshot: 'only-on-failure'
   },
 
